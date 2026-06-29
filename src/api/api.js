70e876bd465153367fa547e04c149c3ca6cdb568
tejo <tejosai363@gmail.com>
2026-06-29 22:00:17 +0530

    updated AI voice ordering

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://digital-menu-adminpage-1.onrender.com/api";
 
 export default API_BASE_URL;
